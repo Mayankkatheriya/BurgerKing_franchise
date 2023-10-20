ee59a30147eacafe4486fa60824eb590e3566d62
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-20 16:04:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let checkBox = [...document.querySelectorAll(`input[type="checkbox"]`)];
+let orderBtn = document.querySelector("#orderBtn");
+let orderId = document.querySelector(".orderId");
+let imageContainer = document.querySelector(".food-img-container");
+let image = document.querySelector(".food-img");
+
+function randomTime() {
+  return Math.floor(Math.random() * 2000) + 2000; //random number between 2000 to 3999
+}
+function randomId() {
+  return Math.floor(Math.random() * 900) + 100; //random id between 0 and 999
+}
+function getTime() {
+  const h = String(new Date().getHours()).padStart("2", "0");
+  const m = String(new Date().getMinutes()).padStart("2", "0");
+  const s = String(new Date().getSeconds()).padStart("2", "0");
+  const date = String(new Date().getDate()).padStart("2", "0");
+  const mon = String(new Date().getMonth() + 1).padStart("2", "0");
+  const year = String(new Date().getFullYear());
+  let time = year + date + mon + h + m + s;
+  return time;
+}
+
+orderBtn.addEventListener("click", (e) => {
+  let selectedFood = [];
+  for (let i = 0; i < checkBox.length; i++) {
+    if (checkBox[i].checked) {
+      selectedFood.push(checkBox[i].value);
+    }
+  }
+  // console.log("selected food", selectedFood);
+  if (selectedFood.length < 1) {
+    alert("Please select atleast one item");
+    return;
+  }
+  //   orderId.style.display = "none";
+  orderId.innerHTML = `Preparing Food...`;
+
+  imageContainer.style.display = "none";
+  orderBtn.disabled = true;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, randomTime());
+  });
+  promise.then(() => {
+    orderId.innerHTML = `<span class = "uniqueId">Your OrderId:</span> ${getTime()}<span class = "myId">${randomId()}</span>`;
+    if (selectedFood.length == 1) {
+      image.src = `./Assets/${selectedFood}.jpg`;
+    } else if (selectedFood.length == 2) {
+      image.src = `./Assets/${selectedFood[0]}${selectedFood[1]}.jpg`;
+    } else if (selectedFood.length == 3) {
+      image.src = `./Assets/${selectedFood[0]}${selectedFood[1]}${selectedFood[2]}.jpg`;
+    } else {
+      image.src = "./Assets/comboall.jpeg";
+    }
+    imageContainer.style.display = "block";
+    orderId.style.display = "block";
+    orderBtn.disabled = false;
+  });
+});
